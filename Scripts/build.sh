e20ed29a745442e1c88414d94ed57c7ca93de82b
Gregory Sims <briggybros.2@googlemail.com>
2017-10-22 18:22:33 +0100

    Made Android build

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,35 +1,14 @@
 #! /bin/sh
 
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+project="disparity"
 
-# Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
-
-echo "Attempting to build $project for Windows"
+echo "Attempting to build $project"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
-
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
-
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+  -buildTarget Android "$(pwd)/Build/android/$project.apk" \
   -quit
 
 echo 'Logs from build'
