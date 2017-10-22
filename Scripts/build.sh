3d59f88e1760925b56c0ee0f30fae893e33b809f
Gregory Sims <briggybros.2@googlemail.com>
2017-10-22 14:26:03 +0100

    Remove silent crashes

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -9,7 +9,6 @@ echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
@@ -19,7 +18,6 @@ echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
@@ -29,7 +27,6 @@ echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
