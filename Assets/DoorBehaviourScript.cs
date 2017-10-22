47f6b8ad799ead24b8e71986f80d344bc6db13a6
Aidan-acad <aidanhball@btinternet.com>
2017-10-22 14:05:41 +0100

    Outline for lever, pad, door interactions created

---
+++ Assets/DoorBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour {
+    bool open;
+	// Use this for initialization
+	void Start () {
+        open = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (open)
+        {
+            //Open the door
+        }
+        else
+        {
+            //do whatever doors do while they wait
+        }
+	}
+
+    public void SetOpenable(bool set)
+    {
+        open = set;
+    }
+}
