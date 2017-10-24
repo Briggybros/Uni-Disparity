caefedddef3d391a48a7ed77f37c43f3c6fc19fc
textfox <ec15201@my.bristol.ac.uk>
2017-10-24 14:51:10 +0100

    basic tank movement

---
+++ assets/character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour {
+
+	void Start () {          // Take the initial position
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)) {        // Left
+			transform.Rotate(Vector3.up, -90, Space.Self);
+		}
+		if(Input.GetKeyDown(KeyCode.D)) {        // Right
+			transform.Rotate(Vector3.up, 90, Space.Self);
+		}
+		if(Input.GetKeyDown(KeyCode.W)) {        // Up
+			transform.Translate(Vector3.forward);
+		}
+		if(Input.GetKeyDown(KeyCode.S)) {        // Down
+			transform.Translate(Vector3.back);
+		}
+
+	}
+
+}
