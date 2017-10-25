20c4ee7687c4fac04339f416dfd81fbd12cf71a0
Aidan-acad <aidanhball@btinternet.com>
2017-10-25 19:38:31 +0100

    Comments noted, inheritance implemented, lever implementation still waiting on player interact command definition

---
+++ Assets/CollisionTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : Trigger {
+    public GameObject owner;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == owner/*ensure it the the player name, or the object name*/){
+            Activate(true);
+        }
+    }
+}
