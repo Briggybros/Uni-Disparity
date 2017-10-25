0fe58fe7e3f933705fa2c0b7075d53256f397d64
Aidan-acad <aidanhball@btinternet.com>
2017-10-25 19:38:31 +0100

    Comments noted, inheritance implemented, lever implementation still waiting on player interact command definition

--- Assets/DoorBehaviourScript.cs
+++ Assets/DoorBehaviourScript.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class DoorBehaviourScript : MonoBehaviour {
     public bool open;
-	// Use this for initialization
-	void Start () {
+
+
+    // Use this for initialization
+    void Start () {
         open = false;
-	}
+    }
 	
-	// Update is called once per frame
-	void Update () {
-        if (open)
-        {
+    // Update is called once per frame
+    void Update () {
+        if (open){
             Debug.Log("Door opened");
             open = false;
             //Open the door
         }
-        else
-        {
+        else{
             //do whatever doors do while they wait
         }
-	}
+    }
 
-    public void SetOpenable(bool set)
+    public void ToggleOpen(bool set)
     {
         open = set;
     }
---
+++ Assets/Trigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+    public GameObject target;
+    public bool active;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active){
+            active = false;
+        }
+    }
+
+    public void Activate(bool set)
+    {
+        active = set;
+    }
+}
