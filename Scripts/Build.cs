46c5ecde5a1f60e7e26885d91bb38d3d4ab64892
Gregory Sims <briggybros.2@googlemail.com>
2017-10-26 14:37:07 +0100

    Added password

---
+++ Scripts/Build.cs
@@ -0,0 +1,8 @@
+﻿using UnityEditor;
+
+class Build {
+    static void PerformBuild() {
+        string[] scenes = { "Assets/TestScene.unity" };
+        BuildPipeline.BuildPlayer(scenes, "Build/android/", BuildTarget.Android, BuildOptions.None);
+    }
+}
