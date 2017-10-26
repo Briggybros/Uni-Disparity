8f6db78229e7661817882486d32d24550d6c94d6
Gregory Sims <briggybros.2@googlemail.com>
2017-10-26 16:58:45 +0100

    Test

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,15 +6,9 @@ echo "Attempting to build $project"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -quit \
-  -username Briggybros \
-  -password Disparity4eva \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -executeMethod Build.PerformBuild
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
-
-ls $(pwd)
-ls $(pwd)/Build/
-ls $(pwd)/Build/android/
