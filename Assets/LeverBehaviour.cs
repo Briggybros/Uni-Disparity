ac975df4588a328c01dbc334a1152094374a141e
Aidan-acad <ab15100@bristol.ac.uk>
2017-11-09 11:45:32 +0000

    conflict resolved

---
+++ Assets/LeverBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverBehaviour : MonoBehaviour {
+    GameObject door;
+    bool active;
+
+	// Use this for initialization
+	void Start () {
+        active = false;
+        door = GameObject.Find("Door"/*door's name*/);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ActivateDoor()
+    {
+        door.GetComponent<DoorBehaviour>().SetOpenable(true);
+    }
+
+    public void ActivateLever(bool set)
+    {
+        active = set;
+    }
+}
---
+++ Assets/PressurePadBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePadBehaviour : MonoBehaviour {
+    GameObject door;
+    bool active;
+    // Use this for initialization
+    void Start () {
+        active = false;
+        door = GameObject.Find("Door"/*door's name*/);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TriggerDoor()
+    {
+        door.GetComponent<DoorBehaviour>().SetOpenable(true);
+    }
+
+    public void ActivatePad(bool set)
+    {
+        active = set;
+    }
+}
