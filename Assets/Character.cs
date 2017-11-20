56486865a6865674e98bdc62aacf2f0d217057ca
Aidan-acad <ab15100@bristol.ac.uk>
2017-11-20 14:18:42 +0000

    Cleaned up some code

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -36,26 +36,20 @@ public class Character : MonoBehaviour {
 
     void OnCollisionEnter(Collision c) {
         if (Parentables.Contains(c.gameObject)) {
-           // HeldScale = this.transform.localScale;
             this.transform.SetParent(c.gameObject.transform, true);
             pos = this.transform.localPosition;
-            //Debug.Log("COLLSIDE");
             rot = this.transform.localRotation;
             MovementSpeed = 0.8f;
             BlockInput = false;
-           // this.transform.localScale = HeldScale;
         }
     }
 
     void OnCollisionExit(Collision c) {
         if (Parentables.Contains(c.gameObject)) {
             this.transform.parent = null;
-            //this.transform.localPosition = new Vector3(4,-13,0);
-            //this.transform.localRotation = Quaternion.identity; 
             pos = this.transform.localPosition;
             rot = this.transform.localRotation;
             MovementSpeed = 4.0f;
-           // this.transform.localScale = HeldScale;
         }
     }
 
@@ -63,9 +57,7 @@ public class Character : MonoBehaviour {
         // the equality checks for rotation and position are overriden by the classes
         // to return equal when they are "close enough" accounting for precision
         // this introduces some error that can compound over time
-        //rot = transform.localRotation;
-        //transform.rotation.eulerAngles.Set(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, 0);
-        this.transform.localRotation.eulerAngles.Set(0, this.transform.localRotation.eulerAngles.y, 0);
+        this.transform.localRotation.eulerAngles.Set(0, this.transform.localRotation.eulerAngles.y, 0); //Force upright
         if (BlockInput && this.GetComponent<Rigidbody>().IsSleeping()) {
             pos = this.transform.localPosition;
             rot = this.transform.localRotation;
@@ -75,21 +67,20 @@ public class Character : MonoBehaviour {
             pos.y = this.transform.localPosition.y;
             pos = this.transform.localPosition;
             rot = this.transform.localRotation;
-            if (Input.GetKey(KeyCode.D)/* && this.transform.localRotation == rot && this.transform.localPosition == pos*/) {
+            if (Input.GetKey(KeyCode.D)) {
                 StopAllCoroutines();
                 rot *= Quaternion.Euler(0, 5, 0);
                 StartCoroutine(Rotate(rot));
             }
 
-            if (Input.GetKey(KeyCode.A)/* && this.transform.localRotation == rot && this.transform.localPosition == pos*/) {
+            if (Input.GetKey(KeyCode.A)) {
                 StopAllCoroutines();
                 rot *= Quaternion.Euler(0, -5, 0);
                 StartCoroutine(Rotate(rot));
 
             }
 
-            if (Input.GetKey(KeyCode.W)/* && this.transform.localRotation == rot && this.transform.localPosition == pos*/) {
-                //Debug.Log("Please Cat, please");
+            if (Input.GetKey(KeyCode.W)) {
                 pos += this.transform.localRotation * Vector3.forward * 0.1f * (MovementSpeed / 4);
             }
 
@@ -99,8 +90,7 @@ public class Character : MonoBehaviour {
             Time.deltaTime * MovementSpeed
             );
 
-            if (Input.GetKeyDown(KeyCode.Space)/* this.transform.localRotation == rot &&*/) {
-                //pos += this.transform.localRotation * Vector3.forward * 2 * (MovementSpeed / 4);
+            if (Input.GetKeyDown(KeyCode.Space)) {
                 this.GetComponent<Rigidbody>().AddForce(Vector3.Scale((this.transform.forward + this.transform.up), new Vector3(6f, 6f, 6f)), ForceMode.Impulse);
                 BlockInput = true;
             }
