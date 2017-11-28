02c4c375dea37cf840ce69084badc7720791432b
Aidan-acad <ab15100@bristol.ac.uk>
2017-11-28 22:43:20 +0000

    Basic tutorial framework created

--- Assets/PulseTrigger.cs
+++ Assets/PulseTrigger.cs
@@ -6,16 +6,25 @@ public class PulseTrigger : CollisionTrigger{
 
 
     protected override void OnTriggerStay(Collider other) {
-        if ((other.gameObject == owner || (playerInteract == true && other.tag == "Player")) && (Input.GetKeyDown(KeyCode.E))) {
-            foreach (GameObject target in base.targets) {
-                target.gameObject.GetComponent<ListenerScript>().BroadcastMessage("PulseFlag");
-            }
-        }
+		if (requiresInteract) {
+			if ((other.gameObject == owner || (playerInteract == true && other.tag == "Player")) && (Input.GetKeyDown(KeyCode.E))) {
+				foreach (GameObject target in base.targets) {
+					target.gameObject.GetComponent<ListenerScript>().BroadcastMessage("PulseFlag");
+				}
+			}
+		} else {
+		}
     }
 
     protected override void OnTriggerEnter(Collider other) {
-
-    }
+		if (!requiresInteract) {
+			if ((other.gameObject == owner || (playerInteract == true && other.tag == "Player"))) {
+				foreach (GameObject target in base.targets) {
+					target.gameObject.GetComponent<ListenerScript>().BroadcastMessage("PulseFlag");
+				}
+			}
+		}
+	}
 
     protected override void OnTriggerExit(Collider other) {
 
--- Assets/SwitchTrigger.cs
+++ Assets/SwitchTrigger.cs
@@ -6,18 +6,32 @@ public class SwitchTrigger: CollisionTrigger {
 
 
     protected override void OnTriggerStay(Collider other) {
-        if ((other.gameObject == owner || (playerInteract == true && other.tag == "Player")) && (Input.GetKeyDown(KeyCode.E))){
-            foreach (GameObject target in base.targets) {
-                target.gameObject.GetComponent<ListenerScript>().BroadcastMessage("SwitchFlag");
-            }
-        }
-    }
+		if (requiresInteract) {
+			if ((other.gameObject == owner || (playerInteract == true && other.tag == "Player")) && (Input.GetKeyDown(KeyCode.E))) {
+				foreach (GameObject target in base.targets) {
+					target.gameObject.GetComponent<ListenerScript>().BroadcastMessage("SwitchFlag");
+				}
+			}
+		}
+	}
 
     protected override void OnTriggerEnter(Collider other) {
-
+		if (!requiresInteract) {
+			if ((other.gameObject == owner || (playerInteract == true && other.tag == "Player"))) {
+				foreach (GameObject target in base.targets) {
+					target.gameObject.GetComponent<ListenerScript>().BroadcastMessage("SwitchFlag");
+				}
+			}
+		}
     }
 
     protected override void OnTriggerExit(Collider other) {
-
-    }
+		if (!requiresInteract) {
+			if ((other.gameObject == owner || (playerInteract == true && other.tag == "Player"))) {
+				foreach (GameObject target in base.targets) {
+					target.gameObject.GetComponent<ListenerScript>().BroadcastMessage("SwitchFlag");
+				}
+			}
+		}
+	}
 }
