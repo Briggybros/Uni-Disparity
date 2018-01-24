8269bd7fb8f14b8dd36b16fb94ed8d41b13e2e4f
Ben Norris <bn15932@my.bristol.ac.uk>
2018-01-24 19:33:15 +0000

    UI now auto-loads itself in the scene
    I did some shit for the third time

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    private bool uiOpen = false;
+    public bool matchWidth = false;
+    public GameObject uiControls;
+
+
+    public bool isOpen()
+    {
+        return uiOpen;
+    }
+
+    void setOpen(bool open)
+    {
+        uiOpen = open;
+    }
+
+	// Use this for initialization
+	void Start () {
+        initialiseUI();
+	}
+
+    GameObject createCanvas(string tag)
+    {
+        GameObject parent = new GameObject(tag);
+        parent.AddComponent<Canvas>();
+        parent.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+        parent.AddComponent<CanvasScaler>();
+        parent.GetComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        parent.GetComponent<CanvasScaler>().referenceResolution = new Vector2(1080, 720);
+        if (matchWidth) parent.GetComponent<CanvasScaler>().matchWidthOrHeight = 0;
+        else parent.GetComponent<CanvasScaler>().matchWidthOrHeight = 1;
+
+        return parent;
+    }
+
+    void initialiseUI()
+    {
+        GameObject daddy = createCanvas("UI");
+        GameObject toInstantiate = uiControls;
+        GameObject instantiated = Instantiate(toInstantiate, new Vector2(0, 0), Quaternion.identity) as GameObject;
+        instantiated.transform.SetParent(daddy.transform, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
