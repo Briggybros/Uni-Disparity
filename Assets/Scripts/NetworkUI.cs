f9f34f6a61054281b6af09a251c687fd52cdb40b
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-01-28 13:09:31 +0000

    Add error handling

--- Assets/Scripts/NetworkUI.cs
+++ Assets/Scripts/NetworkUI.cs
@@ -9,12 +9,15 @@ using UnityEngine.UI;
 public class NetworkUI : MonoBehaviour {
 
 	private const string LEVEL_SELECT_NAME = "Level Select Panel";
+	private const string ERROR_MESSAGE_NAME = "Error Message";
 
 	public GameObject buttonPrefab;
 	public string[] levels;
 
 	private NetworkManager networkManager;
 	private GameObject levelSelectPanel;
+	private GameObject errorMessageObject;
+	private IEnumerator errorHideCoroutine = null;
 
 	private GameObject MakeButton (GameObject container, string text, Vector2 position, UnityAction clickListener = null) {
 		GameObject button = Instantiate(buttonPrefab, new Vector2(0, 0), Quaternion.identity);
@@ -28,12 +31,32 @@ public class NetworkUI : MonoBehaviour {
 		return button;
 	}
 
-	// Use this for initialization
+	private IEnumerator HideError (float timeout) {
+		yield return new WaitForSeconds(timeout);
+		errorMessageObject.SetActive(false);
+		errorHideCoroutine = null;
+	}
+
+	private void ShowError (string message) {
+		if (errorHideCoroutine != null) {
+			StopCoroutine(errorHideCoroutine);
+		}
+		errorMessageObject.GetComponent<Text>().text = message;
+		errorMessageObject.SetActive(true);
+		errorHideCoroutine = HideError(5);
+		StartCoroutine(errorHideCoroutine);
+	}
+
 	void Start () {
 		networkManager = NetworkManager.singleton;
 		foreach (Transform child in transform) {
-			if (child.gameObject.name == LEVEL_SELECT_NAME) {
-				levelSelectPanel = child.gameObject;
+			switch (child.gameObject.name) {
+				case LEVEL_SELECT_NAME:
+					levelSelectPanel = child.gameObject;
+					break;
+				case ERROR_MESSAGE_NAME:
+					errorMessageObject = child.gameObject;
+					break;
 			}
 		}
 	}
@@ -54,7 +77,6 @@ public class NetworkUI : MonoBehaviour {
 				CreateInternetMatch(matchName);
 			});
 		}
-		// Need to clear up buttons
 	}
 
 	public void CreateInternetMatch (string matchName) {
@@ -66,8 +88,9 @@ public class NetworkUI : MonoBehaviour {
 			NetworkServer.Listen(matchInfo, 9000);
 
 			networkManager.StartHost(matchInfo);
+		} else {
+			ShowError("Failed to connect to the match");
 		}
-		// Needs error
 	}
 
 	public void FindInternetMatch (Text textObject) {
@@ -79,16 +102,19 @@ public class NetworkUI : MonoBehaviour {
 		if (success) {
 			if (internetMatches.Count != 0) {
 				networkManager.matchMaker.JoinMatch(internetMatches[0].networkId, "", "", "", 0, 0, OnJoinInternetMatch);
+			} else {
+				ShowError("No matches found. Try a different name");
 			}
-			// Needs no matches
+		} else {
+			ShowError("Unable to find matches");
 		}
-		// Needs error
 	}
 
 	private void OnJoinInternetMatch(bool success, string extendedInfo, MatchInfo matchInfo) {
 		if (success) {
 			networkManager.StartClient(matchInfo);
+		} else {
+			ShowError("Failed to join the match");
 		}
-		// Needs error
 	}
 }
