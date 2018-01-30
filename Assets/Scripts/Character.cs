d553d705535d4aa3387a7bdedb05f6794eb7acca
Ben Norris <bn15932@my.bristol.ac.uk>
2018-01-30 13:47:01 +0000

    removed movement changes, movement is now identical to master

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,166 +3,227 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+[RequireComponent(typeof(Rigidbody))]
+public class Character : NetworkBehaviour
+{
 
-public class Character : NetworkBehaviour {
-
-
-	public float RotationSpeed = 15.0f;
-	public float MovementSpeed = 6.0f;
-	private Vector3 pos;
-    private Vector3 clickPos;
-	private Quaternion rot;
-	private bool BlockInput,Orientating,moving;
-	private float rotSpeed = 20f;
-    private float yLevel;
+    private float RotationSpeed = 15.0f;
+    private float MovementSpeed = 4.0f;
+    private Vector3 pos;
+    private Quaternion rot;
+    public bool BlockInput;
+    public bool interacting;
+    public bool touching;
+    private int count;
+    private NetworkIdentity targetNetworkIdent;
+    private GameObject target;
+    public bool canMove;
 
     private Vector3 HeldScale;
 
-    public List<GameObject> Parentables;
+    //Handles rotation
+    IEnumerator Rotate(Quaternion finalRotation)
+    {
+        while (transform.localRotation != finalRotation)
+        {
+            transform.localRotation = Quaternion.Slerp(
+                transform.localRotation,
+                finalRotation,
+                Time.deltaTime * RotationSpeed
+            );
+            yield return 0;
+        }
+        transform.localRotation = finalRotation;
+    }
 
-	//Handles rotation
-	IEnumerator Rotate(Quaternion finalRotation){
-		while(this.transform.localRotation != finalRotation) {
-			this.transform.localRotation = Quaternion.Slerp(
-				this.transform.localRotation,
-				finalRotation,
-				Time.deltaTime * RotationSpeed
-			);
-			yield return 0;
-		}
-		this.transform.localRotation = finalRotation;
-	}
+    void Start()
+    {
+        rot = transform.localRotation;
+        pos = transform.localPosition;
+        BlockInput = false;
+        interacting = false;
+        touching = false;
+        count = 0;
+        targetNetworkIdent = null;// = this.GetComponent<NetworkIdentity>();
+    }
 
-	void Start () {
-		rot = transform.localRotation;
-		pos = transform.localPosition;
-        clickPos = transform.localPosition;
-		BlockInput = false;
-		Orientating = true;
-	}
+    [Command]
+    void CmdsyncChange(string tag, GameObject target)
+    {
+        targetNetworkIdent.AssignClientAuthority(connectionToClient);
+        RpcupdateState(tag, target);
+        targetNetworkIdent.RemoveClientAuthority(connectionToClient);
+    }
 
-	//Parents on interaction with collider
-    void OnCollisionEnter(Collision c) {
-        if (Parentables.Contains(c.gameObject)) {
-            this.transform.SetParent(c.gameObject.transform, true);
-            pos = this.transform.localPosition;
-            rot = this.transform.localRotation;
-            MovementSpeed = 0.8f;
+    [ClientRpc]
+    void RpcupdateState(string tag, GameObject target)
+    {
+        target.tag = tag;
+    }
+
+    //Parents on interaction with collider
+    void OnCollisionEnter(Collision c)
+    {
+        if (!(c.gameObject.GetComponent<RotatingPlatformBehaviourScript>() == null && c.gameObject.GetComponent<MovingPlatformBehaviour>() == null))
+        {
+            transform.SetParent(c.gameObject.transform.parent.transform, true);
+            pos = transform.localPosition;
+            rot = transform.localRotation;
+            //MovementSpeed = 0.8f;
             BlockInput = false;
         }
-    }
-
-    void OnCollisionExit(Collision c) {
-        if (Parentables.Contains(c.gameObject)) {
-            this.transform.parent = null;
-            pos = this.transform.localPosition;
-            rot = this.transform.localRotation;
-            MovementSpeed = 4.0f;
+        else if ((c.gameObject.GetComponent<Interactable>() != null))
+        {
+            targetNetworkIdent = c.gameObject.GetComponent<NetworkIdentity>();
+            target = c.gameObject;
+            touching = true;
+        }
+        if (c.gameObject.tag == "Enemy")
+        {
+            transform.position = Checkpoint.GetActiveCheckpointPosition();
         }
     }
 
-    void Update () {
+    void OnCollisionExit(Collision c)
+    {
+        if (!(c.gameObject.GetComponent<RotatingPlatformBehaviourScript>() == null && c.gameObject.GetComponent<MovingPlatformBehaviour>() == null))
+        {
+            transform.parent = null;
+            pos = transform.localPosition;
+            rot = transform.localRotation;
+            //MovementSpeed = 4.0f;
+        }
+        else if ((c.gameObject.GetComponent<Interactable>() != null))
+        {
+            interacting = false;
+            target = null;
+            touching = false;
+        }
+    }
+
+    static bool isInteract()
+    {
+#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
+		return Input.GetKeyDown(KeyCode.E);
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
+		return Touch.Test("Use");
+#endif
+    }
+
+    static bool IsRight()
+    {
+#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
+		return Input.GetKey(KeyCode.D);
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
+		return Touch.Test("Right");
+#endif
+    }
+
+    static bool IsLeft()
+    {
+#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
+		return Input.GetKey(KeyCode.A);
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
+		return Touch.Test("Left");
+#endif
+    }
+
+    static bool IsForward()
+    {
+#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
+		return Input.GetKey(KeyCode.W);
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
+		return Touch.Test("Forward");
+#endif
+    }
+
+    static bool IsJump()
+    {
+#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
+		return Input.GetKeyDown(KeyCode.Space);
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
+		return Touch.Test("Jump");
+#endif
+    }
+
+
+    void Update()
+    {
+
+        if (!canMove)
+        {
+            return;
+        }
+
         if (!isLocalPlayer)
             return;
-        this.transform.localRotation.eulerAngles.Set(0, this.transform.localRotation.eulerAngles.y, 0); //Force upright
-
-		//Rigidbody lines control jump start/end
-        if (BlockInput && this.GetComponent<Rigidbody>().IsSleeping()) {
-            pos = this.transform.localPosition;
-            clickPos = pos;
-            rot = this.transform.localRotation;
-            yLevel = this.transform.position.y;
+        transform.localRotation.eulerAngles.Set(0, transform.localRotation.eulerAngles.y, 0); //Force upright
+        if (!interacting && isInteract() && touching)
+        {
+            interacting = true;
+            //this.gameObject.tag = "PlayerInteract";
+            CmdsyncChange("Bopped", target);
+        }
+        if (interacting && !isInteract())
+        {
+            interacting = false;
+        }
+        if (transform.localPosition.y <= -2)
+        {
+            transform.position = Checkpoint.GetActiveCheckpointPosition();
+        }
+        /*if (interacting) {
+			count++;
+			if(count > 2) {
+				//this.gameObject.tag = "Player";
+				CmdsyncChange("Player",target);
+				count = 0;
+			}
+		}*/
+        //Rigidbody lines control jump start/end
+        if (BlockInput && GetComponent<Rigidbody>().IsSleeping())
+        {
+            pos = transform.localPosition;
+            rot = transform.localRotation;
             BlockInput = false;
         }
-        //Falling check
-        if(this.transform.localPosition.y <= yLevel - 2)
+        if (!BlockInput)
         {
-            this.transform.SetPositionAndRotation(Checkpoint.GetActiveCheckpointPosition(), Quaternion.identity);
-            clickPos = this.transform.localPosition;
-            moving = false;
-        }
-        //Some other stuff
-        if (!BlockInput) {
-            pos = this.transform.localPosition;
-            rot = this.transform.localRotation;
-
-            //tap to move script
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Input.GetMouseButtonDown(0))
+            pos.y = transform.localPosition.y;
+            pos = transform.localPosition;
+            rot = transform.localRotation;
+            if (IsRight())
             {
-                if (Physics.Raycast(ray, out hit, 200))
-                {
-                    if (hit.collider.CompareTag("Floor"))
-                    {
-                        if (hit.point.y < this.transform.localPosition.y + 0.1f && hit.point.y > this.transform.localPosition.y - 0.1f)
-                        { 
-                            clickPos = hit.point;
-                            clickPos.y = this.transform.localPosition.y;
-                            Orientating = true;
-                            moving = true;
-                            this.GetComponent<Animator>().SetBool("Running", true);
-                        }
-                    }
-                }
-            }
-
-            //Old wad movement script
-            if (Input.GetKey(KeyCode.D)) {
                 StopAllCoroutines();
                 rot *= Quaternion.Euler(0, 5, 0);
                 StartCoroutine(Rotate(rot));
             }
 
-            if (Input.GetKey(KeyCode.A)) {
+            if (IsLeft())
+            {
                 StopAllCoroutines();
                 rot *= Quaternion.Euler(0, -5, 0);
                 StartCoroutine(Rotate(rot));
 
             }
 
-            if (Input.GetKey(KeyCode.W)) {
-                pos += this.transform.localRotation * Vector3.forward * 0.1f * (MovementSpeed / 4);
+            if (IsForward())
+            {
+                pos += transform.localRotation * Vector3.forward * 0.1f * (MovementSpeed / 4);
             }
 
-            //Movement etc
-			Debug.Log("Orientating state" + Orientating);
-			if(Orientating){
-				if((clickPos - transform.position).magnitude < 0.5){
-					Orientating = false;
-				}else if(Vector3.Angle(clickPos - transform.position,transform.forward) < 1){
-					Orientating = false;
-				}else{
-					transform.rotation = Quaternion.Slerp(
-						transform.rotation,
-						Quaternion.LookRotation(clickPos-transform.position),
-						Time.deltaTime * rotSpeed
-					);
-				}
-			}else{
-	            //Update position
-	            if (Vector3.Distance(this.transform.position, clickPos) != 0)
-	            {
-                    if (moving)
-                    {
-                        this.transform.localPosition = Vector3.MoveTowards(
-                        this.transform.localPosition,
-                        clickPos,
-                        Time.deltaTime * MovementSpeed
-                        );
-                    }
-	            }
-                else
-                {
-                    this.GetComponent<Animator>().SetBool("Running", false);
-                    moving = false;
-                }
-			}
+            //Update position
+            transform.localPosition = Vector3.MoveTowards(
+            transform.localPosition,
+            pos,
+            Time.deltaTime * MovementSpeed
+            );
 
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                this.GetComponent<Rigidbody>().AddForce(Vector3.Scale((this.transform.forward + this.transform.up), new Vector3(6f, 6f, 6f)), ForceMode.Impulse);
+            if (IsJump())
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.Scale((transform.forward + transform.up), new Vector3(6f, 6f, 6f)), ForceMode.Impulse);
                 BlockInput = true;
             }
         }
-	}
+    }
 }
