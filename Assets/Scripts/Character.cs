c2b3ebcfd356ba1799e4897014b304376c227a57
Gregory Sims <briggybros.2@googlemail.com>
2018-02-02 11:59:36 +0000

    Made walking on static work
    Interaction is broken

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,51 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(NetworkIdentity))]
 [RequireComponent(typeof(Rigidbody))]
 public class Character : NetworkBehaviour
 {
-
-    private float RotationSpeed = 15.0f;
-    private float MovementSpeed = 4.0f;
     private Vector3 pos;
     private Quaternion rot;
-    public bool BlockInput;
-    public bool interacting;
-    public bool touching;
     private NetworkIdentity targetNetworkIdent;
-    private GameObject target;
     private float yLevel;
     public bool canMove;
 
     private Vector3 HeldScale;
 
-    //Handles rotation
-    IEnumerator Rotate(Quaternion finalRotation) {
-        while (transform.localRotation != finalRotation) {
-            transform.localRotation = Quaternion.Slerp(
-                transform.localRotation,
-                finalRotation,
-                Time.deltaTime * RotationSpeed
-            );
-            yield return 0;
-        }
-        transform.localRotation = finalRotation;
-    }
-
     void Start() {
         rot = transform.localRotation;
         pos = transform.localPosition;
-        BlockInput = false;
         canMove = true;
-        interacting = false;
-        touching = false;
         targetNetworkIdent = GetComponent<NetworkIdentity>();
     }
 
     [Command]
     void CmdsyncChange(string tag, GameObject target) {
+        Debug.Log("csc: " + tag + ", " + target);
         targetNetworkIdent.AssignClientAuthority(connectionToClient);
         RpcupdateState(tag, target);
         targetNetworkIdent.RemoveClientAuthority(connectionToClient);
@@ -63,12 +42,9 @@ public class Character : NetworkBehaviour
             transform.SetParent(c.gameObject.transform.parent.transform, true);
             pos = transform.localPosition;
             rot = transform.localRotation;
-            //MovementSpeed = 0.8f;
-            BlockInput = false;
-        } else if ((c.gameObject.GetComponent<Interactable>() != null)) {
-            targetNetworkIdent = c.gameObject.GetComponent<NetworkIdentity>();
-            target = c.gameObject;
-            touching = true;
+        }
+        if ((c.gameObject.GetComponent<Interactable>() != null)) {
+            CmdsyncChange("Bopped", c.gameObject);
         }
         if (c.gameObject.tag == "Enemy") {
             transform.position = Checkpoint.GetActiveCheckpointPosition();
@@ -82,61 +58,11 @@ public class Character : NetworkBehaviour
             rot = transform.localRotation;
             //MovementSpeed = 4.0f;
         } else if ((c.gameObject.GetComponent<Interactable>() != null)) {
-            interacting = false;
-            target = null;
-            touching = false;
+            CmdsyncChange("Static", c.gameObject);
         }
     }
-
-    static bool isInteract() {
-#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
-		return Input.GetKeyDown(KeyCode.E);
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
-		return Touch.Test("Use");
-#endif
-    }
-
-    static bool IsRight() {
-#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
-		return Input.GetKey(KeyCode.D);
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
-		return Touch.Test("Right");
-#endif
-    }
-
-    static bool IsLeft() {
-#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
-		return Input.GetKey(KeyCode.A);
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
-		return Touch.Test("Left");
-#endif
-    }
-
-    static bool IsForward() {
-#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
-		return Input.GetKey(KeyCode.W);
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
-		return Touch.Test("Forward");
-#endif
-    }
-
-    static bool IsJump() {
-#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
-		return Input.GetKeyDown(KeyCode.Space);
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE || UNITY_EDITOR
-		return Touch.Test("Jump");
-#endif
-    }
-
 
     void Update() {
-
-        if (!GetComponent<Rigidbody>().IsSleeping()) {
-            GetComponent<Animator>().SetBool("Running", true);
-        } else {
-            GetComponent<Animator>().SetBool("Running", false);
-        }
-
         if (!canMove) {
             return;
         }
@@ -144,68 +70,27 @@ public class Character : NetworkBehaviour
         if (!isLocalPlayer)
             return;
         transform.localRotation.eulerAngles.Set(0, transform.localRotation.eulerAngles.y, 0); //Force upright
-        if (!interacting && isInteract() && touching) {
-            interacting = true;
-            //this.gameObject.tag = "PlayerInteract";
-            CmdsyncChange("Bopped", target);
-        }
-        if (interacting && !isInteract()) {
-            interacting = false;
-        }
+        
         if (transform.localPosition.y <= -2) {
             transform.position = Checkpoint.GetActiveCheckpointPosition();
         }
-        /*if (interacting) {
-			count++;
-			if(count > 2) {
-				//this.gameObject.tag = "Player";
-				CmdsyncChange("Player",target);
-				count = 0;
-			}
-		}*/
-		//Rigidbody lines control jump start/end
-        if (BlockInput && GetComponent<Rigidbody>().IsSleeping()) {
-            pos = transform.localPosition;
-            rot = transform.localRotation;
-            yLevel = this.transform.position.y;
-            BlockInput = false;
-        }
         //Falling check
-        if(this.transform.localPosition.y <= yLevel - 2) {
+        if (this.transform.localPosition.y <= yLevel - 2) {
             this.transform.SetPositionAndRotation(Checkpoint.GetActiveCheckpointPosition(), Quaternion.identity);
         }
-        if (!BlockInput) {
-            pos.y = transform.localPosition.y;
-            pos = transform.localPosition;
-            rot = transform.localRotation;
-            if (IsRight()) {
-                StopAllCoroutines();
-                rot *= Quaternion.Euler(0, 5, 0);
-                StartCoroutine(Rotate(rot));
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(ray, out hit, 200)) {
+                this.GetComponent<NavMeshAgent>().destination = hit.point;
             }
+        }
 
-            if (IsLeft()) {
-                StopAllCoroutines();
-                rot *= Quaternion.Euler(0, -5, 0);
-                StartCoroutine(Rotate(rot));
-
-            }
-
-            if (IsForward()) {
-                pos += transform.localRotation * Vector3.forward * 0.1f * (MovementSpeed / 4);
-            }
-
-			//Update position
-            transform.localPosition = Vector3.MoveTowards(
-            transform.localPosition,
-            pos,
-            Time.deltaTime * MovementSpeed
-            );
-
-            if (IsJump()) {
-                GetComponent<Rigidbody>().AddForce(Vector3.Scale((transform.forward + transform.up), new Vector3(6f, 6f, 6f)), ForceMode.Impulse);
-                BlockInput = true;
-            }
+        if (this.GetComponent<NavMeshAgent>().velocity != Vector3.zero) {
+            this.GetComponent<Animator>().SetBool("Running", true);
+        } else {
+            this.GetComponent<Animator>().SetBool("Running", false);
         }
     }
 }
