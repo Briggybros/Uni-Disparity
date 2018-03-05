a8a4b5634e640f4d8c37f3c7f263f496d944486b
Ben Norris <bn15932@my.bristol.ac.uk>
2018-03-05 21:28:12 +0000

    TextBox support added
    Added textboxes, redid esta's code using code I'd already toold everyone I had.
    Added cat expressions for text boxes.
    Slight changes to other buttons.
    Made changes to menu background colour.
    Preparation for checking textboxes work with a sick new struct called a Chat(tm).

--- Assets/Scripts/TextBoxManager.cs
+++ Assets/Scripts/TextBoxManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class TextBoxManager : MonoBehaviour {
 
-	public GameObject textBox;
+	/**public GameObject textBox;
 
 	public Text theText;
 
@@ -104,5 +104,100 @@ public class TextBoxManager : MonoBehaviour {
 		theText.text = lineOfText;
 		isTyping = false;
 		cancelTyping = false;
-	}
+	}**/
+
+    //Bens code from existing textbox code elsewhere o___o
+
+    public struct Chat
+    {
+        public string[] messages;
+        public Sprite avatar;
+    };
+
+    public Chat[] chats;
+
+    public GameObject textBox;
+
+    public bool repeated;
+
+    private bool opened = false;
+    private bool isOpen = false;
+    private int chat = 0;
+    private int chatMessage = 0;
+
+    public void createChat(Chat[] chats)
+    {
+        GameObject.Find("Menu Canvas").SetActive(false);
+        opened = true;
+        GameObject toInstantiate = textBox;
+        GameObject chatPane = Instantiate(toInstantiate, new Vector2(0, 0), Quaternion.identity) as GameObject;
+        GameObject o = GameObject.Find("ChatImage");
+        o.GetComponent<Image>().sprite = chats[0].avatar;
+        o = GameObject.Find("TextBox");
+        o.GetComponent<Button>().onClick.AddListener(incrementChat);
+        StartCoroutine(updateAnimatedText(chats[0].messages[chatMessage]));
+        isOpen = true;
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && (repeated || !opened))
+        {
+            createChat(chats);
+        }
+    }
+
+    void incrementChat()
+    {
+        chatMessage++;
+        if (chatMessage == chats[chat].messages.Length)
+        {
+            chat++;
+            chatMessage = 0;
+            GameObject pic = GameObject.Find("ChatImage");
+            pic.GetComponent<Image>().sprite = chats[chat].avatar;
+        }
+        if(chat == chats.Length) { 
+            Destroy(GameObject.FindGameObjectWithTag("TextCanvas"));
+            isOpen = false;
+            chat = 0;
+            GameObject.Find("Menu Canvas").SetActive(true);
+        }
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(updateAnimatedText(chats[chat].messages[chatMessage]));
+        }
+    }
+
+    bool updateChat(string message)
+    {
+        GameObject o = GameObject.Find("TextBoxText");
+        if (o != null)
+        {
+            o.GetComponent<Text>().text = message;
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator updateAnimatedText(string strComplete)
+    {
+        int i = 0;
+        string str = "";
+        while (i < strComplete.Length)
+        {
+            str += strComplete[i++];
+            if (updateChat(str))
+            {
+                yield return new WaitForSeconds(0.04F);
+            }
+            else
+            {
+                yield break;
+            }
+        }
+    }
 }
