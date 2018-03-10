dca42150cbfbf625e38d80acc16dde4c5a4892b9
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-03-10 17:59:14 +0000

    Add pause menu prefab and scripts

---
+++ Assets/Scripts/PauseUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PauseUI : MonoBehaviour {
+
+	// Use this for initialization
+	public void OnPause() {
+		Time.timeScale = 0;
+	}
+
+	public void OnResume() {
+		Time.timeScale = 1;
+	}
+
+	public void Disconnect() {
+		NetworkManager.singleton.StopHost();
+		NetworkManager.singleton.onlineScene = null;
+		Time.timeScale = 1;
+	}
+}
