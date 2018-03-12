8fe1361c8c764fe292319f10d9f16c562bde00cf
k4l397 <kyle.jake.kw@gmail.com>
2018-03-12 23:30:46 +0000

    add some structure for different spectate modes Also I hate vulforia, whyyyy are you adding assets D:

--- Assets/Scripts/SpecatorCameraController.cs
+++ Assets/Scripts/SpecatorCameraController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpecatorCameraController : MonoBehaviour {
+	private const int ROTATEWORLD = 1;
+	private const int FOLLOW = 2;
+	private int camera = ROTATEWORLD;
+	public char world;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
-		transform.LookAt(Vector3.zero);
+		if (camera = ROTATEWORLD){
+			transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+			transform.LookAt(Vector3.zero);
+		}
 	}
 }
