8a02244a9210b7ff3cb0577fad31deec085aa915
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-03-13 22:30:05 +0000

    Add a scoreboard prefab for the scoring

--- Assets/Scripts/ScoreboardUI.cs
+++ Assets/Scripts/ScoreboardUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,12 +9,17 @@ public class ScoreboardUI : MonoBehaviour {
 
 	public GameObject ScoreDisplayPrefab;
 
-	void Start () {
-		StartCoroutine(Scoreboard.GetScores((results) => {
+	public void ShowScores (Scoreboard.Score[] scores = null) {
+		Scoreboard.ScoresCallback callback = (results) => {
 			foreach (Scoreboard.Score score in results) {
 				DisplayScore(score);
 			}
-		}, SceneManager.GetActiveScene().name));
+		};
+		if (scores == null) {
+			StartCoroutine(Scoreboard.GetScores(callback, SceneManager.GetActiveScene().name));
+		} else {
+			callback(scores);
+		}
 	}
 
 	private void DisplayScore(Scoreboard.Score score) {
@@ -22,6 +28,7 @@ public class ScoreboardUI : MonoBehaviour {
 		Text[] texts = instance.GetComponentsInChildren<Text>();
 		texts[0].text = score.rank.ToString();
 		texts[1].text = score.name;
-		texts[2].text = score.time.ToString();
+		TimeSpan timeSpan = TimeSpan.FromSeconds(score.time);
+		texts[2].text = string.Format("{0:00}:{1:00}:{2:00}", timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);;
 	}
 }
