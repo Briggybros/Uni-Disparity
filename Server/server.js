94a7eaada6f47e40f0a9088c37a47d37f01bdaa2
Gregory Sims <briggybros.2@googlemail.com>
2018-03-13 18:11:52 +0000

    Fixed up scoreboard server

--- Server/server.js
+++ Server/server.js
@@ -1,40 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sqlite = require('sqlite3');
+
+const scoreboard = require('./scoreboard');
 
 const PORT = 8090;
 
 const app = express();
-const db = new sqlite.Database('./database.sqlite');
-
-db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS scores (name VARCHAR, level VARCHAR, completion_time INT)');
-});
 
 app.use(bodyParser.json());
 
-app.post('/score', async (req, res) => {
-  db.serialize(() => {
-    const statement = db.prepare('INSERT INTO scores (name, level, completion_time) VALUES (?, ?, ?)');
-    statement.run(req.body.name, req.body.level, req.body.time, (err) => {
-      if (err) res.sendStatus(500);
-      else res.sendStatus(200);
-    });
-  });
+app.post('/score', (req, res) => {
+  res.status(200).send(JSON.stringify(scoreboard.insertScore({
+    name: req.body.name,
+    level: req.body.level,
+    time: req.body.time,
+  })));
 });
 
-app.get('/score', async (req, res) => {
-  db.serialize(() => {
-    const statement = db.prepare('SELECT name, completion_time FROM scores ORDER BY score DESC WHERE level=?');
-    statement.all(req.body.level, (err, rows) => {
-      if (err) res.sendStatus(500);
-      else {
-        const { count } = req.query;
-        const results = rows.slice(0, count || rows.length);
-        res.status(200).send(JSON.stringify(results));
-      }
-    });
-  });
+app.get('/score', (req, res) => {
+  res.status(200).send(JSON.stringify(scoreboard.getScores(
+    req.query.level,
+    req.query.offset,
+    req.query.size,
+  )));
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
