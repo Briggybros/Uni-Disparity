0c58d69f45ddc7a5d5960939a8f5d3b7d8dcedef
Aidan-acad <ab15100@bristol.ac.uk>
2018-03-19 10:23:08 +0000

    fixed names and stuff

--- Assets/Scripts/ChestBehaviour.cs
+++ Assets/Scripts/ChestBehaviour.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class ChestBehaviour : MonoBehaviour {
 
 	public GameObject key;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (this.tag == "Bopped") {
