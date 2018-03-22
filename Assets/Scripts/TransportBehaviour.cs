ac01ce9cf84e24aed15e58dc1ba79c3d7f43174f
Gregory Sims <Briggybros@users.noreply.github.com>
2018-03-22 11:19:11 +0000

    Update TransportBehaviour.cs

--- Assets/Scripts/TransportBehaviour.cs
+++ Assets/Scripts/TransportBehaviour.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TransportBehaviour : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
