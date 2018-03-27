faf37084090b3cc43651f8d285562945168833e2
textfox <esta@hotmail.co.uk>
2018-03-27 10:42:39 +0100

    setup started for pin entry

---
+++ Assets/Scripts/PinMechanism.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinMechanism : Receiver {
+    
+    bool StopPlayerMovement;
+    bool OverlayOn;
+    public GameObject PinEntryUI;
+
+	// Use this for initialization
+	void Start () {
+        DisablePinEntry();
+	}
+
+    protected override void SwitchReceived()
+    {
+        Debug.Log("switch triggered");
+        if (OverlayOn == false)
+        {
+            Debug.Log("here");
+            EnablePinEntry();
+        }
+        else if (OverlayOn == true)
+        {
+            DisablePinEntry();
+        }
+    }
+
+    public void EnablePinEntry () {
+        OverlayOn = true;
+        PinEntryUI.SetActive(true);
+    }
+
+    public void DisablePinEntry () {
+        OverlayOn = false;
+        PinEntryUI.SetActive(false);
+    }
+}
