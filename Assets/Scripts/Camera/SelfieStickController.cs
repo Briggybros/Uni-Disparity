fda5656a903fc197c782c758c1e239d7f8eb00c3
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-03-28 18:34:45 +0100

    Only activate spectator cameras on local

--- Assets/Scripts/Camera/SelfieStickController.cs
+++ Assets/Scripts/Camera/SelfieStickController.cs
@@ -1,32 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using Vuforia;
 
 // selfie stick since we essentially stick a camera on the end of a stick move it around
-public class SelfieStickController : MonoBehaviour {
+[RequireComponent(typeof(NetworkIdentity))]
+public class SelfieStickController : NetworkBehaviour {
 
 	private bool automaticControl = false;
 	private SpawnOnTargetDetect spawnOnTargetDetect;
 
 	// Use this for initialization
 	void Start () {
+		if (!isLocalPlayer) {
+			return;
+		}
 		transform.position = Vector3.zero;
 		GameObject.FindWithTag("MainCamera").SetActive(false);
+		foreach (Transform childTransform in transform) {
+			GameObject child = childTransform.gameObject;
+			if (child.GetComponent<Camera>() != null) {
+				child.SetActive(true);
+			}
+		}
 
         GameObject[] canvasComponents = GameObject.FindGameObjectsWithTag("UI");
 		foreach (var component in canvasComponents) {
-			Debug.Log(component.name);
 			if (component.name == "UI Canvas" && CharacterPicker.IsSpectator()) {
             	component.GetComponent<Canvas>().enabled = false;
 			}
 		}
-
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
 		if (automaticControl) {
 			transform.RotateAround(Vector3.zero, Vector3.up, 10 * Time.deltaTime);
 			if(Input.GetKey(KeyCode.Space)) {
