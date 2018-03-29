6da1fa39cf4ecb2e87092884e6d05756a0e21f88
textfox <ec15201@my.bristol.ac.uk>
2018-03-29 10:39:40 +0100

    increased speed

---
+++ Assets/Scripts/OpeningDrawbridge.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningDrawbridge : Receiver {
+
+	public GameObject Drawbridge;
+	private Quaternion newRotation;
+	private bool opened;
+	// Use this for initialization
+	void Start () {
+		opened = false;
+	}
+	
+	// Update is called once per frame
+	protected override void SwitchReceived () {
+		Debug.Log("switch received");
+		StopAllCoroutines();
+		if (opened == false) {
+			opened = true;
+			StartCoroutine(OpenDrawbridge());
+		}
+		else {
+			opened = false;
+			StartCoroutine(CloseDrawbridge());
+		}
+	}
+	
+	IEnumerator OpenDrawbridge () {
+		Debug.Log("setting new rotation");
+		newRotation = Quaternion.Euler(-90, Drawbridge.transform.rotation.y, Drawbridge.transform.rotation.z);
+		Debug.Log("maybe doing the rotation");
+		while (Drawbridge.transform.rotation.x > -90) {
+			Drawbridge.transform.rotation = Quaternion.Lerp(Drawbridge.transform.rotation, newRotation, 2*Time.deltaTime);
+			yield return 0;
+		}
+	}
+
+	IEnumerator CloseDrawbridge () {
+		newRotation = Quaternion.Euler(0, Drawbridge.transform.rotation.y, Drawbridge.transform.rotation.z);
+		while (Drawbridge.transform.rotation.x < 0) {
+			Drawbridge.transform.rotation = Quaternion.Lerp(Drawbridge.transform.rotation, newRotation, 2*Time.deltaTime);
+			yield return 0;
+		}
+	}
+}
