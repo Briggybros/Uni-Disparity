22bb2fc6ad5406d1b6db9784c53949c289ba5567
Gregory Sims <briggybros.2@googlemail.com>
2018-04-14 17:49:20 +0100

    Added trigger to disable navmesh

---
+++ Assets/Scripts/NavMeshHole.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshHole : MonoBehaviour {
+	public Vector3 center;
+	public Vector3 size;
+	public bool initiallyHoley = false;
+
+	private BoxCollider box;
+
+	void Start() {
+		box = this.gameObject.AddComponent(typeof(BoxCollider)) as BoxCollider;
+		box.center = center;
+		box.size = size;
+		box.isTrigger = true;
+		box.enabled = initiallyHoley;
+	}
+
+	public void EnableHole() {
+		box.enabled = true;
+	}
+
+	public void DisableHole() {
+		box.enabled = false;
+	}
+
+	public void OnCollisionEnter(Collision collision) {
+		NavMeshAgent agent = collision.gameObject.GetComponent(typeof(NavMeshAgent)) as NavMeshAgent;
+		Rigidbody body = collision.gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
+		if (agent != null && body != null) {
+			agent.enabled = false;
+			body.isKinematic = true;
+		}
+	}
+}
