d1aedcfda09590734930541f29ac7d5bc6aeb379
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-04-16 20:09:56 +0100

    Add agent component in scene instead of via script
    Removed this which is fake news on C#
    Resize wolf box collider and make extra collider a trigger

--- Assets/Scripts/PatrollingEnemyBehaviour.cs
+++ Assets/Scripts/PatrollingEnemyBehaviour.cs
@@ -5,32 +5,23 @@ using UnityEngine.AI;
 
 public class PatrollingEnemyBehaviour : Receiver {
 	public Vector3[] positions;
-	public float height = 1.0f;
-	public float baseOffset = 0.25f;
-	public float radius = 1.0f;
-	public float angularSpeed = 360.0f;
-	public float positionalSpeed = 3.5f;
 
 	private int destination = 0;
-	private bool moved = false;
+	private bool moved = true;
 	private NavMeshAgent agent;
 
 	// Use this for initialization
 	protected override void Start () {
-		this.agent = this.gameObject.AddComponent(typeof(NavMeshAgent)) as NavMeshAgent;
-		this.agent.height = this.height;
-		this.agent.radius = this.radius;
-		this.agent.angularSpeed = this.angularSpeed;
-		this.agent.baseOffset =	this.baseOffset;
+		agent = GetComponent<NavMeshAgent>();
 	}
 
 	protected override void Update() {
-		if (moved && this.agent.velocity == Vector3.zero) {
-			this.destination++;
-			this.agent.destination = this.positions[this.destination % this.positions.Length];
-			this.moved = false;
+		if (moved && agent.velocity == Vector3.zero) {
+			destination++;
+			agent.destination = positions[destination % positions.Length];
+			moved = false;
 		} else {
-			this.moved = true;
+			moved = true;
 		}
 	}
 }
