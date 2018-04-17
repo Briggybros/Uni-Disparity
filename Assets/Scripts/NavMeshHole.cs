e559f76d95e14393ef3d505ece697324d38dae80
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-04-17 14:39:42 +0100

    add collider in scene

--- Assets/Scripts/NavMeshHole.cs
+++ Assets/Scripts/NavMeshHole.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class NavMeshHole : MonoBehaviour {
-	public Vector3 center;
-	public Vector3 size;
 	public bool initiallyHoley = false;
 
 	private BoxCollider box;
 
 	void Start() {
-		box = this.gameObject.AddComponent(typeof(BoxCollider)) as BoxCollider;
-		box.center = center;
-		box.size = size;
-		box.isTrigger = true;
+		box = GetComponent<BoxCollider>();
 		box.enabled = initiallyHoley;
 	}
 
@@ -31,8 +26,8 @@ public class NavMeshHole : MonoBehaviour {
 	}
 
 	public void OnCollisionEnter(Collision collision) {
-		NavMeshAgent agent = collision.gameObject.GetComponent(typeof(NavMeshAgent)) as NavMeshAgent;
-		Rigidbody body = collision.gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
+		NavMeshAgent agent = collision.gameObject.GetComponent<NavMeshAgent>();
+		Rigidbody body = collision.gameObject.GetComponent<Rigidbody>();
 		if (agent != null && body != null) {
 			agent.enabled = false;
 			body.isKinematic = true;
