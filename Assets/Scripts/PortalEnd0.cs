4e920bbb4dbf7da3aa62f0707f5041f3c5c572bf
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-04-18 14:44:11 +0100

    Add transition

--- Assets/Scripts/PortalEnd0.cs
+++ Assets/Scripts/PortalEnd0.cs
@@ -1,16 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 
 public class PortalEnd0 : Trigger {
 
 	public GameObject end;
-    [SyncVar]
-	private int count;
-
-    private GameObject localPlayer; 
+	public int count;
 	// Use this for initialization
 	protected override void Start () {
 		count = 0;
@@ -20,24 +16,24 @@ public class PortalEnd0 : Trigger {
 	// Update is called once per frame
 	protected override void Update () {
 		if(count > 1) {
-            NetworkManager.singleton.ServerChangeScene("Level 1");
+			NetworkManager.singleton.ServerChangeScene("Level 1");
+			count = 0;
+		}
+		if(count < 0){
 			count = 0;
 		}
 		base.Update();
 	}
 
 	protected virtual void OnTriggerEnter(Collider other) {
-		if (other.tag == "Player") {
-            if (other.gameObject.GetComponent<JoystickCharacter>().isLocalPlayer) {
-                localPlayer = other.gameObject;
-            }
-			count++;
+		if (other.tag == "Player" && this.gameObject != null) {
+			other.gameObject.BroadcastMessage("IncCount",this.gameObject);
 		}
 	}
 
 	protected virtual void OnTriggerExit(Collider other) {
-		if (other.tag == "Player") {
-			count--;
+		if (other.tag == "Player" && this.gameObject != null) {
+			other.gameObject.BroadcastMessage("DecCount",this.gameObject);
 		}
 	}
 }
