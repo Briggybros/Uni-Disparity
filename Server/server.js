a6ca42e83729074a6a871d70d7485b5e054c4c96
Gregory Sims <briggybros.2@googlemail.com>
2018-04-19 13:55:59 +0100

    Added scoreboard

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 const scoreboard = require('./scoreboard');
 
@@ -10,6 +11,8 @@ const PORT = 8090;
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(express.static('./static'));
 
 app.post('/score', (req, res) => {
   res.status(200).send(JSON.stringify(scoreboard.insertScore({
@@ -25,6 +28,19 @@ app.get('/score', (req, res) => {
     req.query.offset,
     req.query.size,
   )));
+});
+
+app.get('/:level', (req, res) => {
+  const pageNumber = req.query.page ? parseInt(req.query.page, 10) : 0;
+  ejs.renderFile('scoreboard.ejs', {
+    scores: scoreboard.getScores(
+      req.params.level,
+      pageNumber * 10,
+      (pageNumber * 10) + 9,
+    ),
+  }, (err, page) => {
+    res.status(200).send(page);
+  });
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
