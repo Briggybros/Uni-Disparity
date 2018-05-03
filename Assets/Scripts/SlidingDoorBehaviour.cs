32f59cbfaecf06ab8b3a368c05b476a29e3f04ba
Aidan-acad <ab15100@bristol.ac.uk>
2018-05-03 14:19:32 +0100

    almost fully playable, back spikes need some testing, extremely hard to get through

--- Assets/Scripts/SlidingDoorBehaviour.cs
+++ Assets/Scripts/SlidingDoorBehaviour.cs
@@ -58,15 +58,14 @@ public class SlidingDoorBehaviour : DoorBehaviourScript {
 	protected override void ColliderWithin(){
 		Debug.Log("sdafsd");
 		if(heldPlayer != null){
-			heldPlayer.BroadcastMessage("Block",gameObject);
+			heldPlayer.BroadcastMessage("Block",this.gameObject);
 		}
 	}
 
      protected override void ColliderExit(){
-	if(heldPlayer != null){
-		heldPlayer.BroadcastMessage("Unblock",gameObject);
-	}
-         
+		if(heldPlayer != null){
+			heldPlayer.BroadcastMessage("Unblock",this.gameObject);
+		}
      }
 
 	protected override void ColliderEnter() {
