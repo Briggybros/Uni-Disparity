966019fa6599fa4a06a5a645537dc1061f0d4bd7
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-05-06 19:03:05 +0100

    Add custom network discovery

---
+++ Assets/Scripts/MyNetworkDiscovery.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkDiscovery : NetworkDiscovery {
+	public void Awake() {
+		useNetworkManager = true;
+	}
+
+	public override void OnReceivedBroadcast(string fromAddress, string data) {
+		Debug.Log("henlo");
+		MyNetworkManager.singleton.networkAddress = fromAddress;
+		MyNetworkManager.singleton.StartClient();
+	}
+}
