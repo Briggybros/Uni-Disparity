b97291cd0771c5bca3861117a87d549eac17c0e4
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-05-07 15:03:45 +0100

    Something with disconnect?

--- Assets/Scripts/PauseUI.cs
+++ Assets/Scripts/PauseUI.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class PauseUI : MonoBehaviour {
+	private NetworkDiscovery discovery;
+
+	public void Start() {
+		discovery = GameObject.Find("Network Manager").GetComponent<NetworkDiscovery>();
+	}
+
 	public void Disconnect() {
+		discovery.StopBroadcast();
 		NetworkManager.singleton.StopHost();
 		NetworkManager.singleton.onlineScene = null;
 		Time.timeScale = 1;
